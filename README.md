5eb1692f3cc9f23eb804ed0af576fdc35f1ba811
George Rolston <grolston@outlook.com>
2021-05-18 11:39:03 -0600

    adding in granularity

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 **ALPHA**
 
+## Action-Level Granularity
+
 ```powershell
 iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/grolston/iam-access-report/master/iamtools.ps1')
+```
+
+## Service-Level Granularity
+
+```powershell
+iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/grolston/iam-access-report/master/iamtools-services.ps1')
 ```
---
+++ iamtools-services.ps1
@@ -0,0 +1,29 @@
+$AccessReport = @()
+$Granularity = 'SERVICE_LEVEL'
+$Roles = Get-IAMAccountAuthorizationDetail -Filter Role
+foreach($role in $Roles.RoleDetailList){
+    $jobId = Request-IAMServiceLastAccessedDetail -Arn $role.Arn -Granularity $Granularity
+    Start-Sleep -Seconds 3
+    $accessDetails = Get-IAMServiceLastAccessedDetail -JobId $jobId
+    foreach ($accessDetail in $accessDetails){
+        if($accessDetail.ServicesLastAccessed.Count -GT 0){
+        #$accessDetail.ServicesLastAccessed
+            foreach($servicedetail in $accessDetail.ServicesLastAccessed){
+              $servicedetail
+                if($servicedetail.TrackedActionsLastAccessed.Count -GT 0){
+                    #$servicedetail.TrackedActionsLastAccessed
+                    # $AccessReport += $servicedetail.TrackedActionsLastAccessed | Select-Object -Property @{label='Type'; expression={"Role"}}, `
+                    #     @{label='Name'; expression={$role.RoleName}}, `
+                    #     @{label='CreateDate'; expression={$role.CreateDate}} , `
+                    #     @{label='IamId'; expression={$role.RoleId}}, `
+                    #     @{label='Arn'; expression={$role.Arn}}, `
+                    #     @{label='ServiceName'; expression={$servicedetail.ServiceName}}, `
+                    #     ActionName, LastAccessedEntity, LastAccessedTime
+                }
+            }
+        }
+    }
+}
+$file = "./IamAccessReport-$($($(Get-Date).ToShortDateString()).Replace('/', '-')).csv"
+$AccessReport | Export-Csv $file -NoTypeInformation
+Write-Host "Download your report at: $file"
